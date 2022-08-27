048d619a5c19b0690a69fc2a701f65c4064f9f01
pvgmenegasso <pvgmenegasso@gmail.com>
2022-08-27 11:41:47 -0300

    Add script to update APT and APT-GET in a single command and restructure project directory structure

---
+++ batch_update/updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# This script executes all update steps automagically
+
+
+sudo apt-get update -y && sudo apt-get upgrade -y && \
+	sudo apt update -y && sudo apt upgrade -y && \
+	echo got here
