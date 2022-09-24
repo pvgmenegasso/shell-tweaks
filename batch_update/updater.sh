eb0e584da0d4d8ac71e32aaa44033c162cef31eb
pvgmenegasso <pvgmenegasso@gmail.com>
2022-09-24 00:43:44 -0300

    Add a script. Fix some typos

--- batch_update/updater.sh
+++ batch_update/updater.sh
@@ -2,6 +2,11 @@
 
 # This script executes all update steps automagically
 
-
+# Update and upgrade apt-get
 sudo apt-get update -y && sudo apt-get upgrade -y && \
-	sudo apt update -y && sudo apt upgrade -y && \
+	# Also update and upgrade apt
+	sudo apt update -y && sudo apt upgrade -y \
+	# Finally update 'refresh' snaps 
+	sudo snap refresh
+
+echo "apt-get, apt and snap updated !"
